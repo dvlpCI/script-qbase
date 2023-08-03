a86340eff58caa1a6bd76afb0f808155c0c17db6
dvlproad <dvlproad@163.com>
2023-08-03 20:43:20 +0800

    【Fix 严重bug】 特别注意：这是qbase。所以无法(强烈不建议)使用 get_package_util.sh 文件，因为qbase.sh处理成qbase二进制文件后，其会被存放到任意路径。就不是相对qbase.sh的路径了。

--- qbase.sh
+++ qbase.sh
@@ -3,7 +3,7 @@
 # @Author: dvlproad
 # @Date: 2023-04-23 13:18:33
  # @LastEditors: dvlproad
- # @LastEditTime: 2023-08-03 19:10:29
+ # @LastEditTime: 2023-08-03 20:40:48
 # @Description:
 ###
 
@@ -47,15 +47,83 @@ if [ "${isTestingScript}" == true ]; then
     args+=("test")
 fi
 
-
-qbaseScriptDir_Absolute="$(cd "$(dirname "$0")" && pwd)"
+# 特别注意：这是qbase。所以无法(强烈不建议)使用 get_package_util.sh 文件，因为qbase.sh处理成qbase二进制文件后，其会被存放到任意路径。就不是相对qbase.sh的路径了。
+# 特别注意：这是qbase。所以无法(强烈不建议)使用 get_package_util.sh 文件，因为qbase.sh处理成qbase二进制文件后，其会被存放到任意路径。就不是相对qbase.sh的路径了。
+# 特别注意：这是qbase。所以无法(强烈不建议)使用 get_package_util.sh 文件，因为qbase.sh处理成qbase二进制文件后，其会被存放到任意路径。就不是相对qbase.sh的路径了。
+# qbaseScriptDir_Absolute="$(cd "$(dirname "$0")" && pwd)"
 # echo "正在执行命令(获取脚本包的版本号):《 sh ${qbaseScriptDir_Absolute}/get_package_util.sh -package \"qbase\" -param \"version\" \"${args[@]}\" 》"
-qbase_latest_version=$(sh ${qbaseScriptDir_Absolute}/get_package_util.sh -package "qbase" -param "version" "${args[@]}")
-# echo "✅✅✅✅ qbase_latest_version=${qbase_latest_version}"
+# qbase_latest_version=$(sh ${qbaseScriptDir_Absolute}/get_package_util.sh -package "qbase" -param "version" "${args[@]}")
+# # echo "✅✅✅✅ qbase_latest_version=${qbase_latest_version}"
 
-# echo "正在执行命令(获取脚本包的根路径):《 sh ${qbaseScriptDir_Absolute}/get_package_util.sh -package \"qbase\" -param \"homedir_abspath\" \"${args[@]}\" 》"
-qbase_homedir_abspath=$(sh ${qbaseScriptDir_Absolute}/get_package_util.sh -package "qbase" -param "homedir_abspath" "${args[@]}")
-# echo "✅✅✅✅ qbase_homedir_abspath=${qbase_homedir_abspath}"
+# # echo "正在执行命令(获取脚本包的根路径):《 sh ${qbaseScriptDir_Absolute}/get_package_util.sh -package \"qbase\" -param \"homedir_abspath\" \"${args[@]}\" 》"
+# qbase_homedir_abspath=$(sh ${qbaseScriptDir_Absolute}/get_package_util.sh -package "qbase" -param "homedir_abspath" "${args[@]}")
+# # echo "✅✅✅✅ qbase_homedir_abspath=${qbase_homedir_abspath}"
+
+function getMaxVersionNumber_byDir() {
+    # 指定目录
+    dir_path="$1"
+
+    # 获取目录下所有文件的列表
+    files=("$dir_path"/*)
+
+    # 从文件列表中筛选出版本号
+    versions=()
+    for file in "${files[@]}"; do
+        version=$(basename "$file" | cut -d "-" -f 2)
+        versions+=("$version")
+    done
+
+    # 选择最新的版本号
+    latest_version=$(echo "${versions[@]}" | tr ' ' '\n' | sort -r | head -n 1)
+    echo "${latest_version}"
+}
+
+function getHomeDir_abspath_byVersion() {
+    # 指定目录
+    dir_path="$1"
+    latest_version="$2"
+
+    # 输出最新版本的路径
+    curretnVersionDir_abspath="$dir_path/$latest_version/bin" # 放在bin目录下
+    if [[ $curretnVersionDir_abspath =~ ^~.* ]]; then
+        # 如果 $curretnVersionDir_abspath 以 "~/" 开头，则将波浪线替换为当前用户的 home 目录
+        curretnVersionDir_abspath="${HOME}${curretnVersionDir_abspath:1}"
+    fi
+    echo "$curretnVersionDir_abspath"
+
+    if [ ! -d "${curretnVersionDir_abspath}" ]; then
+        return 1
+    fi
+}
+function getqscript_allVersionHomeDir_abspath() {
+    requstQScript=$1
+    homebrew_Cellar_dir="$(echo $(which $requstQScript) | sed 's/\/bin\/.*//')"
+    if [ -z "${homebrew_Cellar_dir}" ]; then
+        return 1
+    fi
+
+    if [[ "${homebrew_Cellar_dir}" == */ ]]; then
+        homebrew_Cellar_dir="${homebrew_Cellar_dir::-1}"
+    fi
+    homebrew_Cellar_dir=${homebrew_Cellar_dir}/Cellar
+
+    qscript_allVersion_homedir="${homebrew_Cellar_dir}/$requstQScript"
+    echo "${qscript_allVersion_homedir}"
+}
+
+qtargetScript_allVersion_homedir=$(getqscript_allVersionHomeDir_abspath "qbase")
+qbase_latest_version=$(getMaxVersionNumber_byDir "${qtargetScript_allVersion_homedir}")
+if [ "${isTestingScript}" == true ]; then   # 如果是测试脚本中
+    qbase_homedir_abspath=$(local_test) # 本地测试
+else
+    qbase_homedir_abspath=$(getHomeDir_abspath_byVersion "${qtargetScript_allVersion_homedir}" "${qbase_latest_version}")
+    if [ $? != 0 ]; then
+        exit 1
+    fi
+fi
+
+
+
 
 
 function get_path() {
