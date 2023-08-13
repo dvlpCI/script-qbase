dc1ea875f6eb29389c5d6fa840259ab513d05ecd
dvlproad <dvlproad@163.com>
2023-08-13 16:15:06 +0800

    【Fix】在Mac的shell下，如果你希望打印$a的原始值而不是解释转义字符，你可以使用printf命令而不是echo命令。printf命令可以提供更精确的控制输出格式的能力。

--- log/example_log_msg.sh
+++ log/example_log_msg.sh
@@ -3,7 +3,7 @@
  # @Author: dvlproad
  # @Date: 2023-02-27 14:33:30
  # @LastEditors: dvlproad dvlproad@163.com
- # @LastEditTime: 2023-08-13 02:36:36
+ # @LastEditTime: 2023-08-13 16:12:48
  # @Description: 检查指定Json文件的有效性，是不是合法
  # @example: sh ./json_file_check.sh -checkedJsonF "${Checked_JSON_FILE_PATH}" -scriptResultJsonF "${SCRIPT_RESULT_JSON_FILE}"
 ###
@@ -30,22 +30,14 @@ source ${CurrentDIR_Script_Absolute}/function_log_msg.sh
 
 function log_title() {
     echo "${PURPLE}------------------ $1 ------------------${NC}"
-
-    delay
 }
-
-
-# 延迟20毫秒的函数
-function delay() {
-  local delay=1
-  sleep "$delay"
-}
-
 
 
 str1="❓【33天<font color=warning>@test1</font>】<font color=warning>dev_login_err</font>:<font color=comment>[02.09已提测]</font><font color=comment>@producter1</font><font color=comment>@test1</font>"
 str2="<font color=warning>①登录失败错误提示</font>"
 
+
+log_title "1------------singleString------------"
 log_title "logResultValueToConsole"
 logResultValueToConsole "${str1}\n${str2}"
 if [ $? != 0 ]; then
@@ -55,21 +47,41 @@ fi
 
 
 log_title "logResultValueToFile"
-logResultValueToFile "${str1}\\\n${str2}"
+logResultValueToFile "${str1}\n${str2}"
 if [ $? != 0 ]; then
   exit 1
 fi
 
 
 log_title "logResultValueToJsonFile ✅"
-logResultValueToJsonFile "${str1}\\\n${str2}"
+logResultValueToJsonFile "${str1}\n${str2}"
 if [ $? != 0 ]; then
   exit 1
 fi
 
 
-log_title "logResultValueToJsonFile ❌"
+log_title "logResultValueToJsonFile ✅"
 logResultValueToJsonFile "${str1}\n${str2}"
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+
+
+echo "\n\n"
+log_title "4------------objectString------------"
+objectString="{\"a\":\"${str1}\n${str2}\"}"
+
+log_title "logResultObjectStringToJsonFile"
+logResultObjectStringToJsonFile "${objectString}"
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+
+
+log_title "logResultObjectStringToJsonFile_byJQ"
+logResultObjectStringToJsonFile_byJQ "${objectString}"
 if [ $? != 0 ]; then
   exit 1
 fi
--- log/function_log_msg.sh
+++ log/function_log_msg.sh
@@ -3,10 +3,15 @@
  # @Author: dvlproad
  # @Date: 2023-02-27 14:33:30
  # @LastEditors: dvlproad dvlproad@163.com
- # @LastEditTime: 2023-08-13 02:40:12
+ # @LastEditTime: 2023-08-13 16:13:49
  # @Description: 检查指定Json文件的有效性，是不是合法
- # @example: sh ./json_file_check.sh -checkedJsonF "${Checked_JSON_FILE_PATH}" -scriptResultJsonF "${SCRIPT_RESULT_JSON_FILE}"
+ # @example: 
 ###
+
+# 在Mac的shell下，如果你希望打印$a的原始值而不是解释转义字符，你可以使用printf命令而不是echo命令。printf命令可以提供更精确的控制输出格式的能力。
+# 在Mac的shell下，如果你希望打印$a的原始值而不是解释转义字符，你可以使用printf命令而不是echo命令。printf命令可以提供更精确的控制输出格式的能力。
+# 在Mac的shell下，如果你希望打印$a的原始值而不是解释转义字符，你可以使用printf命令而不是echo命令。printf命令可以提供更精确的控制输出格式的能力。
+
 
 # 定义颜色常量
 NC='\033[0m' # No Color
@@ -17,7 +22,15 @@ BLUE='\033[34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 
+# 延迟20毫秒的函数
+function delay() {
+  local delay=1
+  sleep "$delay"
+}
+
 function logResultValueToConsole() {
+    delay # 延迟,避免其他地方调用该方法，导致输出到同文件名上
+
     # ❌错误方法：使用echo无法正确输出值
     # echo "=============${escaped_value//\\/\\\\}============="
 
@@ -26,7 +39,7 @@ function logResultValueToConsole() {
     TempDir_Absolute="$( cd "$( dirname "$0" )" && pwd )"
     local temp_file_abspath="${TempDir_Absolute}/${now_time}.json"
 
-    echo $1 > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
+    printf "%s" $1 > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
     cat ${temp_file_abspath}
 
     # 删除文件temp_file_abspath
@@ -35,6 +48,8 @@ function logResultValueToConsole() {
 
 
 function logResultValueToFile() {
+    delay # 延迟,避免其他地方调用该方法，导致输出到同文件名上
+
     # ❌错误方法：使用echo无法正确输出值
     # echo "=============${escaped_value//\\/\\\\}============="
 
@@ -43,13 +58,34 @@ function logResultValueToFile() {
     TempDir_Absolute="$( cd "$( dirname "$0" )" && pwd )"
     local temp_file_abspath="${TempDir_Absolute}/${now_time}.json"
 
-    echo $1 > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
+    printf "%s" $1 > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
     cat ${temp_file_abspath}
 
     echo "${YELLOW}更多详情请可点击查看文件:${BLUE}${temp_file_abspath}${NC}"
 }
 
 function logResultValueToJsonFile() {
+    delay # 延迟,避免其他地方调用该方法，导致输出到同文件名上
+    
+    # ❌错误方法：使用echo无法正确输出值
+    # echo "=============${escaped_value//\\/\\\\}============="
+    
+    # ✅正确方法：使用先存到file，再从file中打印
+    local now_time=$(date +"%m%d%H%M%S")
+    TempDir_Absolute="$( cd "$( dirname "$0" )" && pwd )"
+    local temp_file_abspath="${TempDir_Absolute}/${now_time}.json"
+
+
+    printf "%s" '{"testResultValue":"'"$1"'"}' > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
+    # echo $1 > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
+    cat ${temp_file_abspath}
+
+    echo "${YELLOW}更多详情请可点击查看文件:${BLUE}${temp_file_abspath}${NC}"
+}
+
+function logResultObjectStringToJsonFile() {
+    delay # 延迟,避免其他地方调用该方法，导致输出到同文件名上
+
     # ❌错误方法：使用echo无法正确输出值
     # echo "=============${escaped_value//\\/\\\\}============="
 
@@ -59,8 +95,24 @@ function logResultValueToJsonFile() {
     local temp_file_abspath="${TempDir_Absolute}/${now_time}.json"
 
 
-    echo '{"testResultValue":"'"$1"'"}' > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
-    # echo $1 > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
+    printf "%s" '{"testResultValue":'$1'}' > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
+    # # echo $1 > ${temp_file_abspath} # 创建文件，并写入内容到该文件。如果该文件已经存在，则会覆盖原有内容。
+    cat ${temp_file_abspath}
+
+    echo "${YELLOW}更多详情请可点击查看文件:${BLUE}${temp_file_abspath}${NC}"
+}
+
+
+function logResultObjectStringToJsonFile_byJQ() {
+    delay # 延迟,避免其他地方调用该方法，导致输出到同文件名上
+    
+    local now_time=$(date +"%m%d%H%M%S")
+    TempDir_Absolute="$( cd "$( dirname "$0" )" && pwd )"
+    local temp_file_abspath="${TempDir_Absolute}/${now_time}.json"
+
+    
+    printf "%s" "{}" | jq  --argjson jsonString "$1" \
+        '.testResultValue_jq = $jsonString' >> ${temp_file_abspath}   # 这是覆盖，用=，不用+=
     cat ${temp_file_abspath}
 
     echo "${YELLOW}更多详情请可点击查看文件:${BLUE}${temp_file_abspath}${NC}"
