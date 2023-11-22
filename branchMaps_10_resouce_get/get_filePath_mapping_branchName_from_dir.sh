a35171b7f354823eb3e9c1fa333582aa76f092c0
dvlproad <dvlproad@163.com>
2023-11-23 03:23:32 +0800

    【Optimize】在指定目录下获取符合分支名指向的文件及其json内容，未找到返回错误信息

--- branchMaps_10_resouce_get/get_filePath_mapping_branchName_from_dir.sh
+++ branchMaps_10_resouce_get/get_filePath_mapping_branchName_from_dir.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-: <<!
-在指定目录下获取符合分支名指向的文件，未找到返回空字符串
-!
+###
+ # @Author: dvlproad dvlproad@163.com
+ # @Date: 2023-11-23 00:54:34
+ # @LastEditors: dvlproad dvlproad@163.com
+ # @LastEditTime: 2023-11-23 03:22:56
+ # @FilePath: get_filePath_mapping_branchName_from_dir.sh
+ # @Description: 在指定目录下获取符合分支名指向的文件及其json内容，未找到返回错误信息
+### 
 
 # 定义颜色常量
 NC='\033[0m' # No Color
@@ -12,34 +17,10 @@ BLUE='\033[34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 
-
-
-
-
-exit_script() { # 退出脚本的方法，省去当某个步骤失败后，还去继续多余的执行其他操作
-    exit 1
-}
-
-function debug_log() {
-    if [ "${verbose}" == true ]; then
-        echo "$1"
-    fi
-}
-
-# responseJsonString='{
-#     "code": 0
-# }'
-# responseJsonString=$(printf "%s" "$responseJsonString" | jq --arg message "$message" '. + { "message": $message }')
-# printf "%s" "${responseJsonString}"
-
-
 CurCategoryFun_HomeDir_Absolute="$( cd "$( dirname "$0" )" && pwd )"
 qbase_homedir_abspath=${CurCategoryFun_HomeDir_Absolute%/*}   # 使用 %/* 方法可以避免路径上有..
 
-GIT_SCIRTP_DIR_PATH=${qbase_homedir_abspath}/git_content
-qbase_get_fileUrls_inDir_github_scriptPath=${GIT_SCIRTP_DIR_PATH}/get_fileUrls_inDir_github.sh
-qbase_get_fileUrls_inDir_gitee_scriptPath=${GIT_SCIRTP_DIR_PATH}/get_fileUrls_inDir_gitee.sh
-qbase_get_fileUrls_inDir_gitlab_scriptPath=${GIT_SCIRTP_DIR_PATH}/get_fileUrls_inDir_gitlab.sh
+qbase_get_all_json_file_content_inDir_scriptPath=${qbase_homedir_abspath}/git_content/get_all_json_file_content_inDir.sh
 
 # shell 参数具名化
 while [ -n "$1" ]
@@ -53,336 +34,57 @@ do
     esac
 done
 
-# 判断文件是否映射到指定的分支名
-function isFileMappingBranchName() {
-    while [ -n "$1" ]
-    do
-        case "$1" in
-            -branchFile|--branchFile) branchFileAbsolutePathOrUrl=$2; shift 2;;
-            -mappingName|--mappingName) mappingName=$2; shift 2;;
-            -access-token|--access-token) access_token=$2; shift 2;;
-            --) break ;;
-            *) break ;;
-        esac
-    done
-
-
-    if [[ $branchFileAbsolutePathOrUrl == http* ]]; then
-        if [ -z "${access_token}" ]; then
-            echo "${YELLOW}正在执行命令(获取网络地址对应的内容):《${BLUE} curl -s \"$branchFileAbsolutePathOrUrl\" ${YELLOW}》${NC}"
-            FileContent=$(curl -s "$branchFileAbsolutePathOrUrl")
-            if [ $? != 0 ]; then
-                echo "Error❌: 无法获取文件列表。您可能需要添加 -access-token 身份验证令牌参数才能查看此地址内容。"
-                return 1
-            fi
-        else
-            echo "${YELLOW}正在执行命令(获取网络地址对应的内容):《${BLUE} curl -s --header \"Private-Token: $access_token\" \"$branchFileAbsolutePathOrUrl\" ${YELLOW}》${NC}"
-            FileContent=$(curl -s --header "Private-Token: $access_token" "$branchFileAbsolutePathOrUrl")
-            if [ $? != 0 ]; then
-                echo "Error❌: 无法获取文件列表。请检查您的身份验证令牌 ${access_token} 是否正确。"
-                return 1
-            fi
-        fi
-
-        # 检查字符串是否包含指定字符串变量
-        errorFlagString="users/sign_in"
-        if [[ $FileContent == *"$errorFlagString"* ]]; then
-            echo "Error❌: 获取网络地址内容失败。执行的命令为 《 curl -s --header \"Private-Token: $access_token\" \"$branchFileAbsolutePathOrUrl\"》。得到的值为 $FileContent"
-            # echo "所获得的错误内容如下:\n${FileContent}"
-            return 1
-        fi
-    else
-        FileContent=$(cat "$branchFileAbsolutePathOrUrl")
-    fi
-    # 检查文件内容是否符合JSON规范
-    if ! echo "$FileContent" | jq . >/dev/null 2>&1; then
-        echo "您的 ${branchFileAbsolutePathOrUrl} 文件不是一个有效的JSON文件。其内容是 ${FileContent}"
-        return 1
-    fi
-
-    branchName=$(printf "%s" "${FileContent}" | jq -r '.name') # 去除双引号，才不会导致等下等号判断对不上
-    if [ $? != 0 ]; then
-        echo "${RED}Error❌:获取文件${BLUE} ${branchFileAbsolutePathOrUrl} ${RED}中的 ${BLUE}.name ${RED}失败，其可能不是json格式，请检查并修改或移除，以确保获取分支信息的源文件夹${BLUE} $BranceMaps_From_Directory_PATH ${RED}内的所有json文件都是合规的。${NC}";
-        return 1
-    fi
-    # echo "您文件的branchName=${branchName}"
-    # last_field="${mappingName##*/}" # 获取元素的最后一个字段
-    if [ "$mappingName" != "$branchName" ]; then
-        return 1
-    fi
-
-
-    FileContent=$(printf "%s" "$FileContent" | jq --arg fileUrl "$branchFileAbsolutePathOrUrl" '. + { "fileUrl": $fileUrl }')
-
-    # debug_log "恭喜:找到符合 ${mappingName} 的分支信息文件为: ${branchFileAbsolutePathOrUrl}"
-    printf "%s" "${FileContent}"
-}
-
-# 从本地获取
-function getBranchMapsFromDir_local() {
-    # echo "正在执行 $FUNCNAME"
-    mappingBranchName_FilePaths=()
-
-    if [[ $BranceMaps_From_Directory_PATH =~ ^~.* ]]; then
-        # 如果 $BranceMaps_From_Directory_PATH 以 "~/" 开头，则将波浪线替换为当前用户的 home 目录
-        BranceMaps_From_Directory_PATH="${HOME}${BranceMaps_From_Directory_PATH:1}"
-    fi
-    #获取featureBrances文件夹下的所有分支json组成数组，添加到 ${BranchMapAddToJsonFile} 的 ${BranchMapAddToKey} 中
-    if [ ! -d "${BranceMaps_From_Directory_PATH}" ]; then
-        echo "Error❌:您的 -branchMapsFromDir 指向的'map是从哪个文件夹路径获取'的参数值 ${BranceMaps_From_Directory_PATH} 不存在，请检查！"
-        exit_script
-    fi
-
-    if [ -z "${mappingName}" ]; then
-        echo "Error❌:您的 -requestBranchName 指向的'要为哪个分支名获取信息的分支名'的参数值不能为空，请检查！"
-        exit_script
-    fi
-
-
-    mappingBranchName_JsonStrings=""
-    mappingBranchName_JsonStrings+="["
-    for file in "${BranceMaps_From_Directory_PATH}"/*; do
-        if [ ! -f "$file" ]; then
-            continue
-        fi
-        
-        isMappingResult=$(isFileMappingBranchName -branchFile "$file" -mappingName "$mappingName")
-        if [ $? != 0 ]; then
-            continue
-        fi
-        if [ "${mappingBranchName_JsonStrings}" != "[" ]; then
-            mappingBranchName_JsonStrings+=", "
-        fi
-        mappingBranchName_JsonStrings+="${isMappingResult}"
-
-        mappingBranchName_FilePaths[${#mappingBranchName_FilePaths[@]}]=${file}
-    done
-    mappingBranchName_JsonStrings+="]"
-}
-
-function isValidJsonFile() {
-    filePathOrUrl=$1
-    # 判断文件扩展名是否为.json
-    if [[ "${filePathOrUrl##*.}" != "json" ]]; then
-        errorMessage="File is not a .json file"
-        rawFileContent='
-        {
-            "rawFileAbsUrl": "'"${rawFileAbsUrl}"'",
-            "errorMessage": "'"${errorMessage}"'"
-        }
-        '
-        printf "%s" "${rawFileContent}"
-        return 1
-    fi
-
-
-    if [[ $filePathOrUrl == http* ]]; then
-        FileContent=$(curl -s "$filePathOrUrl")
-    else
-        FileContent=$(cat "$filePathOrUrl")
-    fi
-
-    # 检查文件内容是否符合JSON规范
-    if ! echo "$FileContent" | jq . >/dev/null 2>&1; then
-        errorMessage="File content is not valid JSON"
-        rawFileContent='
-        {
-            "rawFileAbsUrl": "'"${rawFileAbsUrl}"'",
-            "errorMessage": "'"${errorMessage}"'"
-        }
-        '
-        printf "%s" "${rawFileContent}"
-        return 1
-    fi
-
-    rawFileContent=$(curl -s "${rawFileAbsUrl}")
-    printf "%s" "${rawFileContent}"
-    return 0
-}
-
-
-function getBranchMapsFromDir_remote_github() {
-    fileDownloadUrlArrayString=$(sh $qbase_get_fileUrls_inDir_github_scriptPath -dirUrl "${BranceMaps_From_Directory_PATH}" -access-token "${access_token}" -inBranchName "${mappingName}")
-    if [ $? != 0 ]; then
-        echo "${fileDownloadUrlArrayString}"    # 此时此值是错误信息
-        exit_script
-    fi
-    fileDownloadUrlArray=(${fileDownloadUrlArrayString})
-    # echo "================fileDownloadUrlArray=${fileDownloadUrlArray[*]}"
-
-
-    mappingBranchName_JsonStrings=""
-    mappingBranchName_JsonStrings+="["
-    mappingBranchName_FilePaths=()
-    fileDownloadUrlCount=${#fileDownloadUrlArray[@]}
-    for((i=0;i<fileDownloadUrlCount;i++));
-    do
-        rawFileAbsUrl=${fileDownloadUrlArray[i]}
-        # echo "$((i+1)).rawFileAbsUrl=${rawFileAbsUrl}"
-
-        # # 检查文件是否映射到指定分支上
-        isMappingResult=$(isFileMappingBranchName -branchFile "$rawFileAbsUrl" -mappingName "$mappingName" -access-token "${access_token}")
-        if [ $? -ne 0 ]; then
-            debug_log "${isMappingResult}"
-            continue
-        fi
-        if [ "${mappingBranchName_JsonStrings}" != "[" ]; then
-            mappingBranchName_JsonStrings+=", "
-        fi
-        mappingBranchName_JsonStrings+="${isMappingResult}"
-
-        mappingBranchName_FilePaths+=("$rawFileAbsUrl")
-    done
-    mappingBranchName_JsonStrings+="]"
-}
-
-
-function getBranchMapsFromDir_remote_gitee() {
-    BranceMaps_From_Directory_PATH=$1
-    mappingName=$2
-
-    fileDownloadUrlArrayString=$(sh $qbase_get_fileUrls_inDir_gitee_scriptPath -dirUrl "${BranceMaps_From_Directory_PATH}" -access-token "${access_token}" -inBranchName "${mappingName}")
-    if [ $? != 0 ]; then
-        echo "${fileDownloadUrlArrayString}"    # 此时此值是错误信息
-        exit_script
-    fi
-
-    fileDownloadUrlArray=(${fileDownloadUrlArrayString})
-    fileCount=${#fileDownloadUrlArray[@]}
-
-
-    mappingBranchName_FilePaths=()
-    file_JsonStrings=""
-    file_JsonStrings+="["
-    mappingBranchName_JsonStrings=""
-    mappingBranchName_JsonStrings+="["
-    for((i=0;i<fileCount;i++));
-    do
-        rawFileAbsUrl=${fileDownloadUrlArray[i]}
-
-        rawFileContent=$(isValidJsonFile "$rawFileAbsUrl")
-        # echo "$((i+1)).${rawFileRelUrl}的内容如下:\n${rawFileContent}"
-        # if [ $? != 0 ]; then
-        #    return 1        
-        # fi
-        if [ $i -gt 0 ]; then
-            file_JsonStrings+=", "
-        fi
-        file_JsonStrings+="${rawFileContent}"
-
-        # 检查文件是否映射到指定分支上
-        isMappingResult=$(isFileMappingBranchName -branchFile "$rawFileAbsUrl" -mappingName "$mappingName")
-        if [ $? -ne 0 ]; then
-            debug_log "${isMappingResult}"
-            continue
-        fi
-        if [ "${mappingBranchName_JsonStrings}" != "[" ]; then
-            mappingBranchName_JsonStrings+=", "
-        fi
-        mappingBranchName_JsonStrings+="${isMappingResult}"
-
-        mappingBranchName_FilePaths+=("$rawFileAbsUrl")
-    done
-    file_JsonStrings+="]"
-    mappingBranchName_JsonStrings+="]"
-
-
-    # echo "file_JsonStrings的值如下:"
-    # echo "${file_JsonStrings}" | jq '.'
-
-    # noCommit_branchJsonStrings=$(printf "%s" "${file_JsonStrings}" | jq '. | map(select(.name == "'"${mappingName}"'"))')
-    # echo "noCommit_branchJsonStrings=${noCommit_branchJsonStrings}"
-}
-
-
-
-function getBranchMapsFromDir_remote_gitlab() {
-    BranceMaps_From_Directory_PATH=$1
-    mappingName=$2
-
-    fileDownloadUrlArrayString=$(sh $qbase_get_fileUrls_inDir_gitlab_scriptPath -dirUrl "${BranceMaps_From_Directory_PATH}" -access-token "${access_token}" -inBranchName "${mappingName}")
-    if [ $? != 0 ]; then
-        echo "${fileDownloadUrlArrayString}"    # 此时此值是错误信息
-        exit_script
-    fi
-
-
-    fileDownloadUrlArray=(${fileDownloadUrlArrayString})
-    json_file_count=${#fileDownloadUrlArray[@]}
-
-    mappingBranchName_FilePaths=()
-    file_JsonStrings=""
-    file_JsonStrings+="["
-    mappingBranchName_JsonStrings=""
-    mappingBranchName_JsonStrings+="["
-    for((i=0;i<json_file_count;i++));
-    do
-        rawFileAbsUrl=${fileDownloadUrlArray[i]}
-        # echo "$((i+1)).${rawFileAbsUrl}"
-
-        rawFileContent=$(isValidJsonFile "$rawFileAbsUrl")
-        # echo "$((i+1)).${rawFileRelUrl}的内容如下:\n${rawFileContent}"
-        # if [ $? != 0 ]; then
-        #    return 1        
-        # fi
-        if [ $i -gt 0 ]; then
-            file_JsonStrings+=", "
-        fi
-        file_JsonStrings+="${rawFileContent}"
-
-        # 检查文件是否映射到指定分支上
-        isMappingResult=$(isFileMappingBranchName -branchFile "$rawFileAbsUrl" -mappingName "$mappingName" -access-token "${access_token}")
-        if [ $? -ne 0 ]; then
-            echo "${isMappingResult}"
-            continue
-        fi
-        if [ "${mappingBranchName_JsonStrings}" != "[" ]; then
-            mappingBranchName_JsonStrings+=", "
-        fi
-        mappingBranchName_JsonStrings+="${isMappingResult}"
-
-        mappingBranchName_FilePaths+=("$rawFileAbsUrl")
-    done
-    file_JsonStrings+="]"
-    mappingBranchName_JsonStrings+="]"
-    debug_log "================fileDownloadUrlArray=${fileDownloadUrlArray[*]}"
-    
-
-    echo "file_JsonStrings的值如下:"
-    echo "${file_JsonStrings}" | jq '.'
-
-    echo "mappingBranchName_FilePaths=${mappingBranchName_FilePaths[*]}"
-
-    # noCommit_branchJsonStrings=$(printf "%s" "${file_JsonStrings}" | jq '. | map(select(.name == "'"${mappingName}"'"))')
-    # echo "noCommit_branchJsonStrings=${noCommit_branchJsonStrings}"
-}
-
-
-
 if [ -z "${mappingName}" ]; then
     echo "Error❌: 请提供要获取信息的分支名（-requestBranchName 参数）。"
-    exit_script
+    exit 1
 fi
 
+DIRECTORY_URL=${BranceMaps_From_Directory_PATH}
+inBranchName=${mappingName}
+allFileContent_JsonStrings=$(sh $qbase_get_all_json_file_content_inDir_scriptPath -dirUrl "${DIRECTORY_URL}" -access-token "${access_token}" -inBranchName "${inBranchName}")
+if [ $? != 0 ]; then
+    echo "${allFileContent_JsonStrings}" #此时此值是错误原因
+    exit 1
+fi
+echo "allFileContent_JsonStrings 的值如下:"
+echo "${allFileContent_JsonStrings}" | jq '.'
 
-if [[ "${BranceMaps_From_Directory_PATH}" == *"https://github.com"* ]]; then
-    # mappingName="optimize/dev_script_pack"
-    # BranceMaps_From_Directory_PATH="https://github.com/dvlpCI/script-qbase/tree/main/branchMaps_10_resouce_get/example/featureBrances"
-    getBranchMapsFromDir_remote_github
 
-elif [[ "${BranceMaps_From_Directory_PATH}" == *"https://gitee"* ]]; then   # https://gitee.com/profile/personal_access_tokens/
-    # mappingName="dev_script_pack"
-    # BranceMaps_From_Directory_PATH="https://gitee.com/dvlpCI/AutoPackage-CommitInfo/tree/master/example_packing_info/featureBrances"
-    getBranchMapsFromDir_remote_gitee "${BranceMaps_From_Directory_PATH}" "${mappingName}"
+# jsonStringWhereJsonMappingBranchName=$(printf "%s" "${allFileContent_JsonStrings}" | jq '. | map(select(.name == "'"${inBranchName}"'"))')
 
-elif [[ "${BranceMaps_From_Directory_PATH}" == *"https://gitlab"* ]]; then
-    mappingName="chore/pack"
-    BranceMaps_From_Directory_PATH="https://gitlab.xihuanwu.com/bojuehui/mobile/mobile_flutter_wish/-/tree/master/bulidScript/featureBrances"
-    getBranchMapsFromDir_remote_gitlab "${BranceMaps_From_Directory_PATH}" "${mappingName}"
+jsonStringWhereJsonMappingBranchName=""
+jsonStringWhereJsonMappingBranchName+="["
+allFileContent_Count=$(printf "%s" "${allFileContent_JsonStrings}" | jq '. | length')
+for((i=0;i<$allFileContent_Count;i++));
+do
+    iFileContentJsonString=$(printf "%s" "${allFileContent_JsonStrings}" | jq ".[${i}]")
+    # echo "$((i+1)).${iFileContentJsonString}"
+    # 判断文件是否映射到指定的分支名
+    branchName=$(printf "%s" "${iFileContentJsonString}" | jq -r '.name') # 去除双引号，才不会导致等下等号判断对不上
+    if [ $? != 0 ]; then
+        branchFileAbsolutePathOrUrl=$(printf "%s" "${iFileContentJsonString}" | jq -r '.fileUrl')
+        echo "${RED}Error❌:获取文件${BLUE} ${branchFileAbsolutePathOrUrl} ${RED}中的 ${BLUE}.name ${RED}失败，其可能不是json格式，请检查并修改或移除，以确保获取分支信息的源文件夹${BLUE} $BranceMaps_From_Directory_PATH ${RED}内的所有json文件都是合规的。${NC}";
+        continue
+    fi
+    echo "$((i+1)).${branchName}"
+    # last_field="${mappingName##*/}" # 获取元素的最后一个字段
+    if [ "$mappingName" != "$branchName" ]; then
+        continue
+    fi
 
+    if [ -n "${jsonStringWhereJsonMappingBranchName}" ]; then
+        jsonStringWhereJsonMappingBranchName+=", "
+    fi
+    jsonStringWhereJsonMappingBranchName+=${iFileContentJsonString}
+done
+jsonStringWhereJsonMappingBranchName+="]"
+
+
+jsonWhereJsonMappingBranchName_count=$(printf "%s" "${jsonStringWhereJsonMappingBranchName}" | jq '. | length')
+if [ "${jsonWhereJsonMappingBranchName_count}" == 0 ]; then
+    echo "您目录 ${BranceMaps_From_Directory_PATH} 下的所有json文件提取成功了，但是未找到匹配 ${mappingName} 分支名的json文件，请检查。"
+    exit 1
 else
-    getBranchMapsFromDir_local
+    # echo "您找到匹配 ${mappingName} 分支名的json文件组成的json数组如下:"
+    printf "%s" "${jsonStringWhereJsonMappingBranchName}"
 fi
-
-
-printf "%s" "${mappingBranchName_JsonStrings}"
---
+++ git_content/get_all_json_file_content_inDir.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+: <<!
+获取目录下所有json文件的内容(如果不是json或者json不合规的不获取)
+!
+
+# 定义颜色常量
+NC='\033[0m' # No Color
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+
+function exit_script() { # 退出脚本的方法，省去当某个步骤失败后，还去继续多余的执行其他操作
+    exit 1
+}
+
+function debug_log() {
+    # 只有直接执行本脚本的时候才能够输出日志，不然如果是形如 echo $(sh xx.sh) 的时候会导致结果值不对
+    # is_Directly_execute_this_script=true
+    if [ "${is_Directly_execute_this_script}" == true ]; then
+        echo "$1"
+    fi
+}
+
+
+CurCategoryFun_HomeDir_Absolute="$( cd "$( dirname "$0" )" && pwd )"
+qbase_homedir_abspath=${CurCategoryFun_HomeDir_Absolute%/*}   # 使用 %/* 方法可以避免路径上有..
+
+GIT_SCIRTP_DIR_PATH=${CurCategoryFun_HomeDir_Absolute}
+qbase_get_fileUrls_inDir_github_scriptPath=${GIT_SCIRTP_DIR_PATH}/get_fileUrls_inDir_github.sh
+qbase_get_fileUrls_inDir_gitee_scriptPath=${GIT_SCIRTP_DIR_PATH}/get_fileUrls_inDir_gitee.sh
+qbase_get_fileUrls_inDir_gitlab_scriptPath=${GIT_SCIRTP_DIR_PATH}/get_fileUrls_inDir_gitlab.sh
+qbase_get_filecontent_git_all_scriptPath=${GIT_SCIRTP_DIR_PATH}/get_filecontent_git_all.sh
+
+while [ -n "$1" ]
+do
+    case "$1" in
+        -dirUrl|--dir-url) DIRECTORY_URL=$2; shift 2;;
+        -access-token|--access-token) access_token=$2; shift 2;;
+        -inBranchName|--in-branch-name) inBranchName=$2 shift 2;;
+        --) break ;;
+        *) break ;;
+    esac
+done
+
+
+# 如果文件内容是json且有效，则返回文件内容；否则提示出错
+function getContentIfJson() {
+    while [ -n "$1" ]
+    do
+        case "$1" in
+            -branchFile|--branchFile) branchFileAbsolutePathOrUrl=$2; shift 2;;
+            -access-token|--access-token) access_token=$2; shift 2;;
+            --) break ;;
+            *) break ;;
+        esac
+    done
+
+
+    if [[ $branchFileAbsolutePathOrUrl == http* ]]; then
+        FileContent=$(sh $qbase_get_filecontent_git_all_scriptPath -fileUrl "${branchFileAbsolutePathOrUrl}" -access-token "${access_token}")
+        if [ $? != 0 ]; then
+            echo "${FileContent}"
+            return 1
+        fi
+    else
+        FileContent=$(cat "$branchFileAbsolutePathOrUrl")
+    fi
+    # 检查文件内容是否符合JSON规范
+    if ! echo "$FileContent" | jq . >/dev/null 2>&1; then
+        echo "您的 ${branchFileAbsolutePathOrUrl} 文件不是一个有效的JSON文件。其内容是 ${FileContent}"
+        return 1
+    fi
+
+    # 为该json添加 fileUrl 字段，便于从json查看是来自哪个文件的json
+    FileContent=$(printf "%s" "$FileContent" | jq --arg fileUrl "$branchFileAbsolutePathOrUrl" '. + { "fileUrl": $fileUrl }')
+
+    printf "%s" "${FileContent}"
+}
+
+# 从本地获取
+function get_fileUrls_inDir_local() {
+    if [[ $DIRECTORY_URL =~ ^~.* ]]; then
+        # 如果 $DIRECTORY_URL 以 "~/" 开头，则将波浪线替换为当前用户的 home 目录
+        DIRECTORY_URL="${HOME}${DIRECTORY_URL:1}"
+    fi
+    #获取featureBrances文件夹下的所有分支json组成数组，添加到 ${BranchMapAddToJsonFile} 的 ${BranchMapAddToKey} 中
+    if [ ! -d "${DIRECTORY_URL}" ]; then
+        echo "Error❌:您的 -dirUrl 指向的'map是从哪个文件夹路径获取'的参数值 ${DIRECTORY_URL} 不存在，请检查！"
+        return 1
+    fi
+
+    fileDownloadUrlArray=()
+    for file in "${DIRECTORY_URL}"/*; do
+        if [ ! -f "$file" ]; then
+            continue
+        fi
+        fileDownloadUrlArray[${#fileDownloadUrlArray[@]}]=${file}
+    done
+    printf "%s" "${fileDownloadUrlArray[*]}"
+}
+
+function isValidJsonFile() {
+    filePathOrUrl=$1
+    # 判断文件扩展名是否为.json
+    if [[ "${filePathOrUrl##*.}" != "json" ]]; then
+        errorMessage="File is not a .json file"
+        rawFileContent='
+        {
+            "rawFileAbsUrl": "'"${rawFileAbsUrl}"'",
+            "errorMessage": "'"${errorMessage}"'"
+        }
+        '
+        printf "%s" "${rawFileContent}"
+        return 1
+    fi
+
+
+    if [[ $filePathOrUrl == http* ]]; then
+        FileContent=$(curl -s "$filePathOrUrl")
+    else
+        FileContent=$(cat "$filePathOrUrl")
+    fi
+
+    # 检查文件内容是否符合JSON规范
+    if ! echo "$FileContent" | jq . >/dev/null 2>&1; then
+        errorMessage="File content is not valid JSON"
+        rawFileContent='
+        {
+            "rawFileAbsUrl": "'"${rawFileAbsUrl}"'",
+            "errorMessage": "'"${errorMessage}"'"
+        }
+        '
+        printf "%s" "${rawFileContent}"
+        return 1
+    fi
+
+    rawFileContent=$(curl -s "${rawFileAbsUrl}")
+    printf "%s" "${rawFileContent}"
+    return 0
+}
+
+
+if [ -z "${inBranchName}" ]; then
+    echo "Error❌:您的 -inBranchName 参数指向的'要获取哪个分支下的文件'的参数值不能为空，请检查！"
+    exit_script
+fi
+
+
+if [[ "${DIRECTORY_URL}" != "/"* ]] &&  [[ "${DIRECTORY_URL}" != "~"* ]] \
+&& [[ "${DIRECTORY_URL}" != *"https://github.com"* ]] && [[ "${DIRECTORY_URL}" != *"https://gitee"* ]] && [[ "${DIRECTORY_URL}" != *"https://gitlab"* ]]; then
+    echo "您输入的 -dirUrl 参数的文件源地址 ${DIRECTORY_URL} 不是 本地地址，也不是 git 地址，请检查。"
+    exit 1
+fi
+
+if [[ "${DIRECTORY_URL}" == *"https://github.com"* ]]; then
+    fileDownloadUrlArrayString=$(sh $qbase_get_fileUrls_inDir_github_scriptPath -dirUrl "${DIRECTORY_URL}" -access-token "${access_token}" -inBranchName "${inBranchName}")
+
+elif [[ "${DIRECTORY_URL}" == *"https://gitee"* ]]; then 
+    fileDownloadUrlArrayString=$(sh $qbase_get_fileUrls_inDir_gitee_scriptPath -dirUrl "${DIRECTORY_URL}" -access-token "${access_token}" -inBranchName "${inBranchName}")
+
+elif [[ "${DIRECTORY_URL}" == *"https://gitlab"* ]]; then
+    fileDownloadUrlArrayString=$(sh $qbase_get_fileUrls_inDir_gitlab_scriptPath -dirUrl "${DIRECTORY_URL}" -access-token "${access_token}" -inBranchName "${inBranchName}")
+
+else
+    fileDownloadUrlArrayString=$(get_fileUrls_inDir_local)
+fi
+
+if [ $? != 0 ]; then
+    echo "${fileDownloadUrlArrayString}"    # 此时此值是错误信息
+    exit_script
+fi
+fileDownloadUrlArray=(${fileDownloadUrlArrayString})
+# echo "================fileDownloadUrlArray=${fileDownloadUrlArray[*]}"
+
+
+allFileContent_JsonStrings=""
+allFileContent_JsonStrings+="["
+fileDownloadUrlCount=${#fileDownloadUrlArray[@]}
+for((i=0;i<fileDownloadUrlCount;i++));
+do
+    rawFileAbsUrl=${fileDownloadUrlArray[i]}
+    # echo "$((i+1)).rawFileAbsUrl=${rawFileAbsUrl}"
+
+    rawFileContent=$(getContentIfJson -branchFile "$rawFileAbsUrl" -access-token "${access_token}")
+    if [ $? -ne 0 ]; then
+        debug_log "${rawFileContent}"
+        continue
+    fi
+    # echo "$((i+1)).${rawFileRelUrl}的内容如下:\n${rawFileContent}"
+    if [ "${allFileContent_JsonStrings}" != "[" ]; then
+        allFileContent_JsonStrings+=", "
+    fi
+    allFileContent_JsonStrings+="${rawFileContent}"
+done
+allFileContent_JsonStrings+="]"
+
+printf "%s" "${allFileContent_JsonStrings}"
