ef0abafec1e577bc50c77454acf7ad83c9e063a1
dvlproad <dvlproad@163.com>
2026-04-11 17:44:37 +0800

    【Feature】qhelloworld : 一个测试 homebrew tap 的示例

---
+++ qhelloworld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "-------- begin --------"
+
+echo "完整 Homebrew Tap 创建流程: https://github.com/dvlproad/AI-qskills/blob/main/script-to-qbase/SKILL.md"
+
+echo "-------- end --------"
