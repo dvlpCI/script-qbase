beff6cad097eac5d4eb305a134b3f12c8451c600
dvlproad <dvlproad@163.com>
2026-05-15 20:56:00 +0800

    【Feature】env_file_change.sh 环境变量的设置可通过选择环境变量列表来控制

--- env_variables/env_file_change.sh
+++ env_variables/env_file_change.sh
@@ -5,7 +5,7 @@
  # @LastEditors: dvlproad
  # @LastEditTime: 2023-06-14 10:45:11
  # @Description: 对指定环境变量进行设置(设置时候，先添加环境变量及其占位值，然后通过①从文件中选择[如果有传文件的话]或者②终端输入来指定占位值要修改为的指定值。终端输入也会有示例的演示输入）
-# @FilePath: sh env_variables/env_file_change.sh --env-name QTOOL_DEAL_PROJECT_PARAMS_FILE_PATH --env-descript qtool可操作的项目操作列表 --env-var-placeholder your_project_choices_json_file --example-json-file \"/Users/qian/Project/Github/script-branch-json-file/test/tool_choice.json\" --default-output-filename example_env_keys_menu.json --action change --choose-from-env-keys-file-path $QTOOL_DEAL_PROJECT_CHOICES_PATH
+# @FilePath: sh env_variables/env_file_change.sh --env-name QTOOL_DEAL_PROJECT_PARAMS_FILE_PATH --env-descript qtool可操作的项目操作列表 --env-var-placeholder your_project_choices_json_file --example-json-file \"/Users/qian/Project/Github/script-branch-json-file/test/tool_choice.json\" --default-output-filename example_env_keys_menu.json --action change --choose-from-env-keys-file-path /Users/qian/Project/Github/script-qbase/example_env_keys_menu.json
 ### 
 
 CurrentDIR_Script_Absolute="$( cd "$( dirname "$0" )" && pwd )"
@@ -74,15 +74,12 @@ while [ $# -gt 0 ]; do
             shift 2
             ;;
         --choose-from-env-keys-file-path)
-            # 允许空值或者不传：检查下一个参数是否为空或者是选项（以 - 开头）
             if [ -z "$2" ] || [[ "$2" =~ ^- ]]; then
-                # 没有提供值，或者下一个参数是选项，则设置为空
-                CHOOSE_FROM_ENV_KEYS_FILE_PATH=""
-                shift 1  # 只消费当前参数
-            else
-                CHOOSE_FROM_ENV_KEYS_FILE_PATH="$2"
-                shift 2
+                echo "错误: --choose-from-env-keys-file-path 需要指定一个文件路径" >&2
+                exit 1
             fi
+            CHOOSE_FROM_ENV_KEYS_FILE_PATH="$2"
+            shift 2
             ;;
         --action)
             if [ -z "$2" ] || [[ "$2" =~ ^- ]]; then
@@ -301,12 +298,11 @@ function get_selected_env_value_for_selected_env_key() {
     
     local valid_option=false
     while [ "$valid_option" = false ]; do        
-        local selected_value
         selected_value=$(_selectEnvValueForKey "$json_file" "$selected_env_key")
         if [ $? -ne 0 ] || [ -z "$selected_value" ]; then
             log_color_info "未获取到有效的值，请重新选择。"
         else
-            log_color_info "为${BLUE} ${selected_env_key} ${NC}选中的环境变量值为 ${selected_value}"
+            log_color_info "${PURPLE}为${BLUE} ${selected_env_key} ${PURPLE}选中的环境变量值为${BLUE} ${selected_value} ${PURPLE}。${NC}"
             valid_option=true
             break
         fi
@@ -435,132 +431,6 @@ _selectEnvValueForKey() {
     fi
 }
 
-# 更新tool处理的项目
-showProjectList() {
-    env_keys_file_path=$1
-
-    printf "支持的项目列表： (详见: ${YELLOW}${env_keys_file_path}${NC})\n"
-    choiceCount=$(cat "$env_keys_file_path" | jq '.choice|length')
-    for ((i = 0; i < ${choiceCount}; i++)); do
-        iChoiceMap=$(cat "$env_keys_file_path" | jq ".choice" | jq -r ".[${i}]")
-
-        iChoiceOptionId="$((i + 1))"
-        iChoiceName=$(echo "$iChoiceMap" | jq -r ".name")
-
-        
-        iChoiceProjectToolFilePath=$(echo "$iChoiceMap" | jq -r ".project_tool_file_path")
-        #echo "iChoiceProjectToolFilePath=${iChoiceProjectToolFilePath}" >&2
-
-        if [ ! -f "${iChoiceProjectToolFilePath}" ]; then
-            iChoiceProjectDirPath="该项目指向的开启的菜单json文件不存在"
-        else
-            iChoiceProjectDirPath_rel_toolFile_dir=$(cat "${iChoiceProjectToolFilePath}" | jq -r ".project_path.home_path_rel_this_dir")
-            iChoiceProjectDirPath=$(sh $qbase_homedir_abspath/path_util/get_dirpath_by_relpath.sh --file_or_dir_path "${iChoiceProjectToolFilePath}" --rel_path "${iChoiceProjectDirPath_rel_toolFile_dir}")
-        fi
-        #echo "iChoiceProjectDirPath========${iChoiceProjectDirPath}" >&2
-        
-        printf "${GREEN}%-2s%-20s(路径为 ${YELLOW}%s)${NC}\n" "${iChoiceOptionId}" "${iChoiceName}" "${iChoiceProjectDirPath}"
-        printf "${NC}                          %s${NC}\n" "${iChoiceProjectToolFilePath}"
-    done
-}
-
-updateToolDealProject() {
-    env_keys_file_path=$1
-
-    valid_option=false
-    while [ "$valid_option" = false ]; do
-        target_value="${!ENV_NAME}"
-        if [ -z "${target_value}" ]; then
-            read -r -p "您还未选择想要操作的项目，请先选择想要操作的项目的编号(若要退出请输入Q|q) : " option
-        else
-            read -r -p "请选择您想要更换成的项目的编号(若要退出请输入Q|q) : " option
-        fi
-
-        if [ "${option}" == q ] || [ "${option}" == "Q" ]; then
-            exit 2
-        fi
-
-        choiceCount=$(cat "$env_keys_file_path" | jq '.choice|length')
-        targetChoiceCountMap=""
-        hasFound=false
-        for ((i = 0; i < ${choiceCount}; i++)); do
-            iChoiceMap=$(cat "$env_keys_file_path" | jq ".choice" | jq -r ".[${i}]")
-
-            iChoiceName=$(echo "$iChoiceMap" | jq -r ".name")
-            iChoiceOptionId="$((i + 1))"
-
-            if [ "${option}" = ${iChoiceOptionId} ] || [ "${option}" == ${iChoiceName} ]; then
-                targetChoiceCountMap=$iChoiceMap
-                hasFound=true
-                break
-            fi
-        done
-
-        if [ ${hasFound} == true ] && [ -n "${targetChoiceCountMap}" ]; then
-            update_env_vars
-            if [ $? != 0 ]; then
-                return 1
-            fi
-            break
-        else
-            echo "无此选项，请重新输入。"
-        fi
-    done
-}
-
-update_env_vars() {
-    project_tool_file_path=$(echo "$targetChoiceCountMap" | jq -r ".project_tool_file_path")
-    if [ ! -f "${project_tool_file_path}" ]; then
-        printf "${RED}选择项目失败：您从 ${ENV_NAME} 中选择的 $targetChoiceCountMap 的 ${BLUE}project_tool_file_path ${RED}指向的文件 ${YELLOW}${project_tool_file_path}${RED} 文件不存在，无法完成选择，请先检查和修改后，重新执行选择。${NC}\n"
-        return 1
-    fi
-
-    sh $qbase_homedir_abspath/env_variables/env_var_add_or_update.sh -envVariableKey "${CHOOSE_FROM_ENV_KEYS_FILE_PATH}" -envVariableValue "${project_tool_file_path}" --environment-file-auto-open false
-    if [ $? != 0 ]; then
-        return 1
-    fi
-}
-
-validate_choices_json() {
-    local file_path="$1"
-
-    jq '.' "$file_path" > /dev/null 2>&1
-    if [ $? != 0 ]; then
-        printf "${RED}${file_path} 不是有效的 JSON 格式，请检查。${NC}\n"
-        exit 1
-    fi
-
-    local has_choice
-    has_choice=$(jq 'if (.choice? | type) == "array" then true else false end' "$file_path")
-    if [ "${has_choice}" != "true" ]; then
-        printf "${RED}${file_path} 中缺少 .choice 字段或不是数组格式，请检查。${NC}\n"
-        exit 1
-    fi
-
-    local choice_count
-    choice_count=$(jq '.choice | length' "$file_path")
-    if [ "${choice_count}" -eq 0 ]; then
-        printf "${RED}${file_path} 中的 .choice 数组为空，请添加项目后再试。${NC}\n"
-        exit 1
-    fi
-
-    for ((i = 0; i < choice_count; i++)); do
-        local item_name
-        item_name=$(jq -r ".choice[${i}].name" "$file_path")
-        if [ -z "${item_name}" ] || [ "${item_name}" == "null" ]; then
-            printf "${RED}${file_path} 格式错误：第 $((i+1)) 条缺少 name${NC}\n"
-            exit 1
-        fi
-        local item_path
-        item_path=$(jq -r ".choice[${i}].project_tool_file_path" "$file_path")
-        if [ -z "${item_path}" ] || [ "${item_path}" == "null" ]; then
-            printf "${RED}${file_path} 格式错误：第 $((i+1)) 条(${item_name}) 缺少 project_tool_file_path${NC}\n"
-            exit 1
-        fi
-    done
-}
-
-
 # 快速检查是否已设置环境变量，未设置则添加并占位，之后我们还会去修改占位为指定的值
 # 抑制 open（打开编辑器）：避免多次打开的时候，看不到最后一次的最新内容，而是第一次打开时候的内容
 # 注意： env_file_change.sh 的逻辑是添加环境变量前会先添加占位，然后才修改占位为指定的值。
@@ -568,21 +438,21 @@ validate_choices_json() {
 #                          因为等一下可以用户还会选择修改环境变量的值。所以应该等到你决定完是否将环境变量的占位符更新为指定的值后才去打开。
 #                          否则如果占位时候就打开，则修改完占位后打开的看到还是旧值，因为根本没再打开
 # echo "正在执行命令《 sh $qbase_homedir_abspath/env_variables/env_file_check_and_set.sh --env-name \"${ENV_NAME}\" --env-descript \"${ENV_DESCRIPT}\" --env-var-placeholder \"${ENV_VAR_PLACEHOLDER}\" --example-json-file \"${EXAMPLE_JSON_FILE}\" --default-output-filename \"${DEFAULT_OUTPUT_FILENAME}\" --environment-file-auto-open false 》 "
-checkResult=$(sh $qbase_homedir_abspath/env_variables/env_file_check_and_set.sh \
-    --env-name "${ENV_NAME}" \
-    --env-descript "${ENV_DESCRIPT}" \
-    --env-var-placeholder "${ENV_VAR_PLACEHOLDER}" \
-    --example-json-file "${EXAMPLE_JSON_FILE}" \
-    --default-output-filename "${DEFAULT_OUTPUT_FILENAME}" \
-    --environment-file-auto-open false
-)
-if [ $? -ne 0 ]; then
-    echo "${checkResult}"
-    open_sysenv_file
-    exit 2
-fi
-# echo "${checkResult}" >&2   # 注释调试代码，用于查看调试信息
-ENV_NAME_VALUE=${checkResult} # 注意：此处一定要获取更新后的值，不然一定是执行 env_file_check_and_set.sh 前的旧值
+# checkResult=$(sh $qbase_homedir_abspath/env_variables/env_file_check_and_set.sh \
+#     --env-name "${ENV_NAME}" \
+#     --env-descript "${ENV_DESCRIPT}" \
+#     --env-var-placeholder "${ENV_VAR_PLACEHOLDER}" \
+#     --example-json-file "${EXAMPLE_JSON_FILE}" \
+#     --default-output-filename "${DEFAULT_OUTPUT_FILENAME}" \
+#     --environment-file-auto-open false
+# )
+# if [ $? -ne 0 ]; then
+#     echo "${checkResult}"
+#     open_sysenv_file
+#     exit 2
+# fi
+# # echo "${checkResult}" >&2   # 注释调试代码，用于查看调试信息
+# ENV_NAME_VALUE=${checkResult} # 注意：此处一定要获取更新后的值，不然一定是执行 env_file_check_and_set.sh 前的旧值
 # log_color_info "您的环境变量值 ${ENV_NAME}=\"${ENV_NAME_VALUE}\""
 
 if [ "${ACTION}" == "check" ]; then
